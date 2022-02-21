07ae43b7e4db248e29698309dcaa1190ca48b1cb
Samuel Santos <samuellima280499@gmail.com>
2022-02-21 20:02:48 -0300

    Co-authored-by: Allan Garcia <allanfsgarcia@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+let messages = [];
+let userBoxMessages = document.querySelector("#box-messages");
+
+
+function showMessage() {
+
+}
+
 function getChatMessages() {
-  const linkApi = "https://run.mocky.io/v3/74207643-6171-40fc-86bb-ca2588e5d97e";
+  const linkApi = "https://run.mocky.io/v3/e8156de8-7bb9-4a2d-8bd7-7fcdd91c2c03";
+
 
   fetch(linkApi)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-    })
+      messages = data;
 
-  
+      console.log(data);
+      data.forEach(message => {
+        userBoxMessages.innerHTML += `
+          <section class="user-message">
+            <strong class="user-username">
+              ${message.userWhoSent.username} falou:
+            </strong>
+        
+            <span>
+              ${message.content}
+            </span>
+          </section>`;
+    
+      })
+    });
 }
 
 function getChatPeople() {
-  const linkApi = "https://run.mocky.io/v3/0d5fc185-ff53-42ab-abc1-ee2a8d4457d0";
+  const linkApi = "https://run.mocky.io/v3/bb916e64-a07f-4ab3-a486-8b24c9bbff30";
 
   fetch(linkApi)
     .then(response => response.json())
@@ -25,6 +47,8 @@ function sendMessage() {
 }
 
 // Criar função de enviar
+
+setInterval(getChatMessages, 1000);
+
 sendMessage();
-getChatPeople()
 getChatMessages();
